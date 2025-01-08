dddfcfe6ff35534032337349187406a20f2d7211
Dmitry Zhifarsky <dimannich2380@gmail.com>
2025-01-08 17:42:13 +0400

    build: update version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/CQLabs/homebrew-dcm/releases/download/1.25.1/dcm-windows-release.zip'
+$url        = 'https://github.com/CQLabs/homebrew-dcm/releases/download/1.26.0/dcm-windows-release.zip'
 $packageArgs = @{
   packageName   = 'dcm'
   unzipLocation = $toolsDir
   fileType      = 'exe'
   url           = $url
   softwareName  = 'DCMAnalyzer'
-  checksum      = 'd41c2175bf84c4965e800610b802b790'
+  checksum      = 'af61a1559dd3237b1a0ffcc462a57083'
   checksumType  = 'md5'
 }
 Install-ChocolateyZipPackage @packageArgs
